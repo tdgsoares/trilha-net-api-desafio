364de5a587983c0b34467d69dcf3d716fab2baaf
TDGS <taisa.tdgs@gmail.com>
2023-11-01 17:03:52 -0300

    Solucao Desafio Api

--- Models/Tarefa.cs
+++ Models/Tarefa.cs
@@ -3,9 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+
 namespace TrilhaApiDesafio.Models
 {
-    public class Tarefa
+    public class Tarefa 
     {
         public int Id { get; set; }
         public string Titulo { get; set; }
